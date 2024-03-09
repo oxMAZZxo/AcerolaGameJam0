3a80a2c9e679d8e2505cae88a1d94cf601ec8d12
Mazz <117194710+oxMAZZxo@users.noreply.github.com>
2024-03-09 00:37:59 +0000

    Fixed Some lighting

--- The Hunter/Assets/Scripts/GameManager.cs
+++ The Hunter/Assets/Scripts/GameManager.cs
@@ -1,14 +1,20 @@
 using Cinemachine;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
+    [Header("Camera")]
     [SerializeField]private GameObject waterRender;
     [SerializeField]private PolygonCollider2D mainAreaCameraBoundary;
     [SerializeField]private PolygonCollider2D caveAreaCameraBoundary;
     [SerializeField]private CinemachineConfiner2D cameraConfiner;
+    [Header("Lighting")]
+    [SerializeField]private Light2D globalLight;
+    [SerializeField,Range(0,1f)]private float overworldLighting;
+    [SerializeField,Range(0,1f)]private float caveLighting;
     private bool isInOverworld = true;
     private Location playerLocation = Location.Overworld;
 
@@ -31,11 +37,13 @@ public class GameManager : MonoBehaviour
             waterRender.SetActive(false);
             cameraConfiner.m_BoundingShape2D = caveAreaCameraBoundary;
             isInOverworld = false;
+            globalLight.intensity = caveLighting;
         }else
         {
             waterRender.SetActive(true);
             cameraConfiner.m_BoundingShape2D = mainAreaCameraBoundary;
             isInOverworld = true;
+            globalLight.intensity = overworldLighting;
         }
     }
 
